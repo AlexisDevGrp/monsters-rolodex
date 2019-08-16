79ac7d5b2402336806f1b1e60b373312f3e91e04
Alexis Brochier <alexisdevgrp@gmail.com>
2019-08-16 13:45:10 +0100

    Updates

---
+++ precache-manifest.61ea13d7cac9e5b2ef22a90d839c9b49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75a8ea7c56fa373f515427440cf8d20",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "54ca45f9f6e6537bf350",
+    "url": "/monsters-rolodex/static/css/main.6495aeb2.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "54ca45f9f6e6537bf350",
+    "url": "/monsters-rolodex/static/js/main.c8f57135.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
